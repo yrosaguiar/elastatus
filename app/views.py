d12288f4fb9ddd1aa798b5b9347391039b27f72d
yros aguiar <yrosaguiar@gmail.com>
2017-09-01 07:23:57 -0300

    Add unauthorized excepetion and error page

--- app/views.py
+++ app/views.py
@@ -19,67 +19,104 @@ def index():
 @elastatus.route('/<account>/<region>/ec2')
 @validate_account_and_region
 def ec2(account, region):
-    c = connect(account, region, 'ec2')
-    instances = c.get_only_instances()
-    return render_template('ec2.html', region=region, instances=instances)
+    try:
+        c = connect(account, region, 'ec2')
+        instances = c.get_only_instances()
+        return render_template('ec2.html', region=region, instances=instances)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 @elastatus.route('/<account>/<region>/ami')
 @validate_account_and_region
 def ami(account, region):
-    c = connect(account,region, 'ec2')
-    amis = c.get_all_images(owners = ['self'])
-    ami_list = {ami: c.get_image(ami.id) for ami in amis}
-    return render_template('ami.html', region=region, amis=ami_list)
+    try:
+        c = connect(account,region, 'ec2')
+        amis = c.get_all_images(owners = ['self'])
+        ami_list = {ami: c.get_image(ami.id) for ami in amis}
+        return render_template('ami.html', region=region, amis=ami_list)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
+
 
 
 @elastatus.route('/<account>/<region>/ebs')
 @validate_account_and_region
 def ebs(account, region):
-    c = connect(account, region, 'ebs')
-    volumes = c.get_all_volumes()
-    return render_template('ebs.html', volumes=volumes)
+    try:
+        c = connect(account, region, 'ebs')
+        volumes = c.get_all_volumes()
+        return render_template('ebs.html', volumes=volumes)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/snapshots')
 @validate_account_and_region
 def snapshots(account, region):
-    c = connect(account, region, 'ec2')
-    snapshots = c.get_all_snapshots(owner='self')
-    return render_template('snapshots.html', region=region, snapshots=snapshots)
-
+    try:
+        c = connect(account, region, 'ec2')
+        snapshots = c.get_all_snapshots(owner='self')
+        return render_template('snapshots.html', region=region, snapshots=snapshots)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 @elastatus.route('/<account>/<region>/autoscale')
 @validate_account_and_region
 def autoscale(account, region):
-    c = connect(account, region, 'autoscale')
-    asg = c.get_all_groups()
-    return render_template('asg.html', region=region, asg=asg)
+    try:
+        c = connect(account, region, 'autoscale')
+        asg = c.get_all_groups()
+        return render_template('asg.html', region=region, asg=asg)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/elb')
 @validate_account_and_region
 def elb(account, region):
-    c = connect(account, region, 'elb')
-    elb = c.get_all_load_balancers()
-    return render_template('elb.html', region=region, elb=elb)
+    try:
+        c = connect(account, region, 'elb')
+        elb = c.get_all_load_balancers()
+        return render_template('elb.html', region=region, elb=elb)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
+
 
 
 @elastatus.route('/<account>/<region>/sg/<id>')
 @validate_account_and_region
 def sg(account, region, id):
-    c = connect(account, region,'ec2')
-    sg = c.get_all_security_groups(filters={'group-id': id})
-    sg = add_description(sg)
-    return render_template('sg.html', region=region, sg=sg)
+    try:
+        c = connect(account, region,'ec2')
+        sg = c.get_all_security_groups(filters={'group-id': id})
+        sg = add_description(sg)
+        return render_template('sg.html', region=region, sg=sg)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
+
+
+@elastatus.route('/<account>/<region>/ami')
+@validate_account_and_region
+def ami(account, region):
+    try:
+        c = connect(account,region, 'ec2')
+        amis = c.get_all_images(owners = ['self'])
+        ami_list = {ami: c.get_image(ami.id) for ami in amis}
+        return render_template('ami.html', region=region, amis=ami_list)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/elasticache')
 @validate_account_and_region
 def elasticache(account, region):
-    c = connect(account, region, 'elasticache')
-    clusters = c.describe_cache_clusters(show_cache_node_info=True)
-    clusters = clusters['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters']
-    return render_template('elasticache.html', region=region, clusters=clusters)
+    try:
+        c = connect(account, region, 'elasticache')
+        clusters = c.describe_cache_clusters(show_cache_node_info=True)
+        clusters = clusters['DescribeCacheClustersResponse']['DescribeCacheClustersResult']['CacheClusters']
+        return render_template('elasticache.html', region=region, clusters=clusters)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/route53')
@@ -95,42 +132,56 @@ def route53(account, region):
             paginate = records.next_token
     except:
         domain = None
+        return render_template('unauthorized.html', account=account)
     return render_template('r53.html', domain=domain, records=r)
 
 
 @elastatus.route('/<account>/<region>/rds')
 def rds(account, region):
-    c = connect(account, region, 'rds')
-    db_instances = c.get_all_dbinstances()
-    return render_template('rds.html', db_instances=db_instances)
+    try:
+        c = connect(account, region, 'rds')
+        db_instances = c.get_all_dbinstances()
+        return render_template('rds.html', db_instances=db_instances)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/dynamodb')
 def dynamodb(account, region):
-    c = connect(account, region, 'dynamodb')
-    tables = c.list_tables()
-    if tables:
-        tables = [c.describe_table(x) for x in tables]
-    else:
-        tables = list()
-    return render_template('dynamodb.html', tables=tables)
+    try:
+        c = connect(account, region, 'dynamodb')
+        tables = c.list_tables()
+        if tables:
+            tables = [c.describe_table(x) for x in tables]
+        else:
+            tables = list()
+        return render_template('dynamodb.html', tables=tables)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
+
 
 
 @elastatus.route('/<account>/<region>/cloudformation')
 def cloudformation(account, region):
-    c = connect(account, region, 'cloudformation')
-    stacks = c.describe_stacks()
-    return render_template('cloudformation.html', stacks=stacks)
+    try:
+        c = connect(account, region, 'cloudformation')
+        stacks = c.describe_stacks()
+        return render_template('cloudformation.html', stacks=stacks)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/cloudformation/<stack_name>.json')
 def get_template(account, region, stack_name):
-    c = connect(account, region, 'cloudformation')
-    template = c.get_template(stack_name)
-    template = template["GetTemplateResponse"]["GetTemplateResult"]["TemplateBody"]
-    response = make_response(template)
-    response.headers["Content-Disposition"] = "attachment; filename=%s.json" % stack_name
-    return response
+    try:
+        c = connect(account, region, 'cloudformation')
+        template = c.get_template(stack_name)
+        template = template["GetTemplateResponse"]["GetTemplateResult"]["TemplateBody"]
+        response = make_response(template)
+        response.headers["Content-Disposition"] = "attachment; filename=%s.json" % stack_name
+        return response
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/cloudwatch')
@@ -140,35 +191,48 @@ def cloudwatch(account, region):
 
 @elastatus.route('/<account>/<region>/sns')
 def sns(account, region):
-    c = connect(account, region, 'sns')
-    subscriptions = c.get_all_subscriptions()
-    subscriptions = subscriptions['ListSubscriptionsResponse']['ListSubscriptionsResult']['Subscriptions']
-    return render_template('sns.html', subscriptions=subscriptions)
+    try:
+        c = connect(account, region, 'sns')
+        subscriptions = c.get_all_subscriptions()
+        subscriptions = subscriptions['ListSubscriptionsResponse']['ListSubscriptionsResult']['Subscriptions']
+        return render_template('sns.html', subscriptions=subscriptions)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/redshift')
 def redshift(account, region):
-    c = connect(account, region, 'redshift')
-    clusters = c.describe_clusters()
-    clusters = clusters['DescribeClustersResponse']['DescribeClustersResult']['Clusters']
-    return render_template('redshift.html', clusters=clusters)
+    try:
+        c = connect(account, region, 'redshift')
+        clusters = c.describe_clusters()
+        clusters = clusters['DescribeClustersResponse']['DescribeClustersResult']['Clusters']
+        return render_template('redshift.html', clusters=clusters)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/sqs')
 def sqs(account, region):
-    c = connect(account, region, 'sqs')
-    queues = list()
-    all_queues = c.get_all_queues()
-    for q in all_queues:
-        url = 'https://sqs.%s.amazonaws.com%s' % (region, q.id)
-        attributes = q.get_attributes()
-        attributes['url'] = url
-        queues.append(attributes)
-    return render_template('sqs.html', queues=queues)
+    try:
+        c = connect(account, region, 'sqs')
+        queues = list()
+        all_queues = c.get_all_queues()
+        for q in all_queues:
+            url = 'https://sqs.%s.amazonaws.com%s' % (region, q.id)
+            attributes = q.get_attributes()
+            attributes['url'] = url
+            queues.append(attributes)
+        return render_template('sqs.html', queues=queues)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
 
 
 @elastatus.route('/<account>/<region>/sgaudit')
 def sgaudit(account, region):
-    c = connect(account, region, 'ec2')
-    report, empty_groups = get_reports(c)
-    return render_template('sgaudit.html', report=report)
+    try:
+        c = connect(account, region, 'ec2')
+        report, empty_groups = get_reports(c)
+        return render_template('sgaudit.html', report=report)
+    except Exception, e:
+        return render_template('unauthorized.html', account=account)
+
